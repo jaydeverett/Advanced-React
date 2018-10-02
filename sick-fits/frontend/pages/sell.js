a72a75868c310291b2ffe00712d6ee429d93d32d
Jay Deverett <jayjdeverett@gmail.com>
2018-10-01 20:58:00 -0400

    add initial layout

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Sell = () => {
+  return (
+    <div>
+      <p>sell!</p>
+      <Link href="/">
+        <a>Home!</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sell;
